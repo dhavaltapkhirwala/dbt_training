598be3884e119cb415ddbdbe4814abbaba4c053f
Dhaval Tapkhirwala <dhavaltapkhirwala@gmail.com>
2025-05-08 16:04:10 +0000

    re-installing dbtlabs.dbtutils package, and using surrogate key macro

---
+++ models/surrogate_key_usage.sql
@@ -0,0 +1,10 @@
+with orders as (
+    select * from {{ source('jaffle_shop', 'orders') }}
+)
+select
+    {{ dbt_utils.generate_surrogate_key(['user_id','order_date']) }} as id,
+    user_id,
+    order_date,
+    count(1)
+from orders
+group by user_id,order_date
